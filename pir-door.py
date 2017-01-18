bd04cd7022c115a6d368e3d0602231a6bedcf026
Jakkawad <sunnysunoc@gmail.com>
2017-01-18 12:10:45 +0700

    Update pir-door.py

--- pir-door.py
+++ pir-door.py
@@ -3,14 +3,19 @@ import RPi.GPIO as GPIO
 GPIO.setmode(BPIO.BCM)
  
 pir_pin = 18 #GPIO18
-led_pin = 23 #GPIO23
+#led_pin = 23 #GPIO23
  
 GPIO.setup(pir_pin, GPIO.IN)         # activate input
-GPIO.setup(led_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # activate input with PullUp
+#GPIO.setup(led_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # activate input with PullUp
  
 while True:
-    if GPIO.input(pir_pin):
-        print("PIR ALARM!")
-    if GPIO.input(led_pin):
-        print("DOOR ALARM!")
+    value = GPIO.input(pir_pin)
+    if :
+        print value
+    else:
+        print 'Value = nil'
+#    if GPIO.input(pir_pin):
+#        print("PIR ALARM!")
+#    if GPIO.input(led_pin):
+#        print("DOOR ALARM!")
     time.sleep(0.5)
