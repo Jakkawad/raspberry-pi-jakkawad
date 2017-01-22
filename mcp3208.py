cae1a2037e14849d2f4be44a7e9998dec9a559b3
Jakkawad <sunnysunoc@gmail.com>
2017-01-22 13:43:20 +0700

    Update mcp3208.py

--- mcp3208.py
+++ mcp3208.py
@@ -3,7 +3,7 @@ import spidev, time
 spi = spidev.SpiDev()
 
 spi.open(0,0)
-
+//
 def analog_read(channel):
 
     r = spi.xfer2([4 | 2 |(channel>>2), (channel &3) << 6,0])
@@ -15,10 +15,10 @@ def analog_read(channel):
 
 while True:
     LCD1602.init(0x27, 1)
-    reading = analog_read(0)
+    reading = analog_read(0) #อ่านค่าอนาล็อคขาที่ 0 แล้วเอาไปเก็บใน reading
     print reading
-    result = (reading * 100) / 4095
+    result = (reading * 100) / 4095 #หาค่าเปอร์เซ็นของค่า reading แล้วเก็บไว้ใน result
     print result
-    LCD1602.write(0,0, "Mositure Level")
-    LCD1602.write(1,1, "%f"%result)
+    LCD1602.write(0,0, "Gas Level") #ปริ้นค่าออกทาง LCD1602
+    LCD1602.write(1,1, "%f"%result) #แสดงค่าในรูปแบบทศนิยมสองต่ำแหน่ง
     time.sleep(1)
